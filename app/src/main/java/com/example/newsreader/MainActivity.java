166fb0b867d23212df5d41d19edba5ffa1d0a906
satyam <2019335@iiitdmj.ac.in.com>
2021-01-11 16:57:19 +0530

    Final Commit

--- app/src/main/java/com/example/newsreader/MainActivity.java
+++ app/src/main/java/com/example/newsreader/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.newsreader;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -17,30 +22,59 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity {
     ArrayList<String> titles = new ArrayList<String>();
+    ArrayList<String> content = new ArrayList<String>();
     ArrayAdapter arrayAdapter;
+    SQLiteDatabase articleDB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        articleDB = this.openOrCreateDatabase("Articles", MODE_PRIVATE,null);
+        articleDB.execSQL("CREATE TABLE IF NOT EXISTS articles (id INTEGER PRIMARY KEY, title VARCHAR , content VARCHAR)");
+       ////////////////////////////
 
         DownloadTask downloadTask = new DownloadTask();
         try {
             downloadTask.execute("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty");
         }
         catch (Exception e){
-            e.printStackTrace();
+            //e.printStackTrace();
         }
 
 
-        ListView listView =findViewById(R.id.listView);
+        ListView listView = findViewById(R.id.listView);
         arrayAdapter  = new ArrayAdapter(this , android.R.layout.simple_list_item_1,titles);
         listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(),ArticleActivity.class);
+                intent.putExtra("content",content.get(position));
+                startActivity(intent);
+            }
+        });
+        UpdatListView ();
     }
 
-    public class DownloadTask extends AsyncTask<String,Void,String>{
+    public void UpdatListView (){
+        Cursor c = articleDB.rawQuery("SELECT * FROM articles",null);
+        int contentIndex = c.getColumnIndex("content");
+        int titleIndex     = c.getColumnIndex("title");
+        if(c.moveToFirst()){
+            content.clear();
+            titles.clear();
+            do {
+                titles.add(c.getString(titleIndex));
+                content.add(c.getString(contentIndex));
+            }
+            while (c.moveToNext());
+                arrayAdapter.notifyDataSetChanged();
+        }
+    }
+
+    public class DownloadTask extends AsyncTask < String , Void ,  String> {
 
         @Override
         protected String doInBackground(String... urls) {
@@ -63,9 +97,11 @@ public class MainActivity extends AppCompatActivity {
                 if(jsonArray.length()<20){
                     noOfItem = jsonArray.length();
                 }
+                articleDB.execSQL("DELETE FROM articles");   ///why??????(DOUBTS
                 for(int i=0;i<noOfItem;i++){
-                    String itemID = jsonArray.getString(i);
-                    url = new URL("https://hacker-news.firebaseio.com/v0/item/"+itemID+".json?print=pretty");
+                    ////
+                    String articleID = jsonArray.getString(i);
+                    url = new URL("https://hacker-news.firebaseio.com/v0/item/"+articleID+".json?print=pretty");
                     urlConnection = (HttpURLConnection) url.openConnection();
                      inputStream = urlConnection.getInputStream();
                      inputStreamReader = new InputStreamReader(inputStream);
@@ -77,7 +113,8 @@ public class MainActivity extends AppCompatActivity {
                         data = inputStreamReader.read();
                     }
                     JSONObject jsonObject = new JSONObject(Articleinfo);
-                    if((!jsonObject.isNull("title")) && (!jsonObject.isNull("url")) ) {
+                    if(!jsonObject.isNull("title") && !jsonObject.isNull("url")) {
+                        //////
                         String Title = jsonObject.getString("title");
                         String Articleurl = jsonObject.getString("url");
 
@@ -92,18 +129,32 @@ public class MainActivity extends AppCompatActivity {
                             ArticleContent += current;
                             data = inputStreamReader.read();
                         }
-                        Log.i("AticleContent" , ArticleContent);
-                       // Log.i("TitleAndUrl=====:", Title + Articleurl+ "\n");
+//////////////////
+                      // Log.i("AticleContent" , ArticleContent);
+
+                        String sql = "INSERT INTO articles (articleid,title,content) VALUES (? , ? , ?)";
+                        SQLiteStatement statement = articleDB.compileStatement(sql);
+                        statement.bindString(1,articleID);
+                        statement.bindString(2,Title);
+                        statement.bindString(3,ArticleContent);
+                        statement.execute();
+                       //Log.i("TitleAndUrl", Title + Articleurl);
                     }
                 }
 
-                Log.i("urls", result );
+               // Log.i("urls", result );
                 return result;
             }
             catch (Exception e){
                 e.printStackTrace();
             }
             return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            UpdatListView ();
         }
     }
 }
